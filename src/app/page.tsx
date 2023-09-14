9ff49f47bb9f66246a592035df4b24ba6b12b810
John Leskas <john.leskas@gmail.com>
2023-09-15 02:19:28 +0300

    initial nextjs13 setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <div>
+          <h1># ReImagine</h1>
+          <h2>Mozilla ReImagine Rise 25 website project</h2>
+      </div>
+
+    </main>
+  )
+}

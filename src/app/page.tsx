e96b24e03e862af94793441894f6da7374736737
John Leskas <john.leskas@gmail.com>
2023-09-15 20:51:20 +0300

    feat: main layout container implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
-          <h1># ReImagine</h1>
-          <h2>Mozilla ReImagine Rise 25 website project</h2>
+        <h1># ReImagine</h1>
+        <h2>Mozilla ReImagine Rise 25 website project</h2>
       </div>
-
     </main>
   )
 }

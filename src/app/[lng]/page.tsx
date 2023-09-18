688d3133661f66cca58f2ff605d23537c9476ac5
John Leskas <john.leskas@gmail.com>
2023-09-18 14:53:23 +0300

    chore: overall updates

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,15 +1,14 @@
 import { useTranslation } from '../i18n'
+import { HeroBanner } from '../components/hero-banner'
 import { EventsList } from '../components/events-list'
 
 export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng)
+
   return (
     <main className="flex min-h-screen flex-col">
 
-      <h1>{t('title')}</h1>
-      <h1># ReImagine</h1>
-      <h2>Mozilla ReImagine Rise 25 website project</h2>
-
+      <HeroBanner />
       <EventsList />
 
     </main>

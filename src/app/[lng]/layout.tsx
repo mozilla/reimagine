0438b67f432c340b4a9e53ecba65d80e893866f5
John Leskas <john.leskas@gmail.com>
2023-09-20 01:58:32 +0300

    chore: add metadata

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -2,10 +2,10 @@ import '../globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Script from 'next/script';
+import { dir } from 'i18next'
 import { Header } from '../components/header'
 import { Footer } from '../components/footer'
 
-import { dir } from 'i18next'
 import { languages } from '../i18n/settings'
 import { useTranslation } from '../i18n'
 import { NavigationItem } from '../components/header/types'
@@ -13,8 +13,8 @@ import { NavigationItem } from '../components/header/types'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Reimagine the Internet - Mozilla',
-  description: '',
+  title: 'Reclaim the Internet – Reimagining Our Digital Future',
+  description: 'Description: Mozilla invites you to an in-person event to shape its destiny and celebrate visionary voices who champion a more ethical, responsible and inclusive web. Together, let’s reclaim the internet.',
 }
 
 export async function generateStaticParams() {
@@ -35,12 +35,9 @@ export default async function RootLayout({
   const { t } = await useTranslation(lng);
   const navigationItems = t('menuItems', { returnObjects: true }) as NavigationItem[];
 
-  console.log(navigationItems)
-
-
   return (
     <html lang={lng} dir={dir(lng)}>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-6P1MW6NND8"/>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-6P1MW6NND8" />
       <Script strategy="afterInteractive" id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];

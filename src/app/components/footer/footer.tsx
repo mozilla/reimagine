212ff967ad5d59754d6ce07d7b701377e2df6384
John Leskas <john.leskas@gmail.com>
2023-09-19 23:17:12 +0300

    refactor: replace language dropdown with existing library

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -3,12 +3,18 @@
 import React from "react"
 import Link from 'next/link';
 import Image from "next/image";
+import { useRouter, usePathname } from 'next/navigation'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faYoutube,
     faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import { LanguageSelector } from "../language-selector"
+import {
+    Menu,
+    MenuList,
+    MenuButton,
+    MenuItem,
+} from "@reach/menu-button";
 
 const navigation = [
     { name: 'Website Privacy Notice', href: 'https://mozilla.org/privacy/websites/' },
@@ -29,18 +35,55 @@ const socialLinks = [
     },
 ];
 
+const locales = ["en", "de"];
+
 export const Footer = () => {
+    const router = useRouter()
+    const pathname = usePathname()
+    const currentLocale = pathname.replace(/^\//, '');
+    const validLocale = locales.includes(currentLocale) ? currentLocale : "en";
+    let languageNames = new Intl.DisplayNames([validLocale], { type: "language" });
+
+    const handleLocaleChange = (locale) => {
+        if (currentLocale !== locale) {
+            router.push(locale)
+        }
+    }
+
     return (
         <footer className="bg-black">
             <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-                <div className="flex justify-center space-x-6 md:order-2">
-                    {socialLinks.map((item, index) => (
-                        <Link key={index} href={item.href} className="" >
-                            {item.icon}
-                        </Link>
-                    ))}
+                <div className="flex flex-col space-x-6 gap-4 md:order-2 max-w-[100px]">
 
-                    <LanguageSelector />
+                    <div className="language-selector">
+                        <p className="font-bold">Language</p>
+                        <div className="text-black">
+                            <Menu>
+                                <MenuButton>
+                                    <span>
+                                        {locales.includes(currentLocale) ? languageNames.of(currentLocale) : ""}
+                                    </span>
+                                    <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1.25 1.75L6.5 7L11.75 1.75" stroke="#9595A2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                </MenuButton>
+                                <MenuList>
+                                    {locales.map((locale, index) => (
+                                        <MenuItem key={index} onSelect={() => handleLocaleChange(locale)}>{languageNames.of(locale)}</MenuItem>
+                                    ))}
+                                </MenuList>
+                            </Menu>
+                        </div>
+                    </div>
+
+                    <div className="inline-flex gap-6">
+                        {socialLinks.map((item, index) => (
+                            <Link key={index} href={item.href} className="" >
+                                {item.icon}
+                            </Link>
+                        ))}
+                    </div>
+
                 </div>
                 <div className="mt-8 md:order-1 md:mt-0 flex flex-col gap-6">
                     <Link href="https://foundation.mozilla.org/" target="_blank" rel="nofollow noreferrer">
